9218b3df7685bb54da5d2f674158b0f8ab12161d
Jahir Medina <jahir.medina@pm.me>
2022-06-08 17:52:06 -0500

    -Add Awesome Font and Hide/Show Option

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+
+function hideComponent(id) {
+	var x = document.getElementById(id);
+	if (x.style.display === "none") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+  }
+
+function printWeb() {
+	window.print();
+}
