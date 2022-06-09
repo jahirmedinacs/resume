3121e7784ccc69a0589e23672e7769e067c71159
Jahir Medina <jahir.medina@pm.me>
2022-06-08 20:40:39 -0500

    -Add Bio\n - Add Work Experience

--- assets/main.js
+++ assets/main.js
@@ -3,8 +3,10 @@ function hideComponent(id) {
 	var x = document.getElementById(id);
 	if (x.style.display === "none") {
 	  x.style.display = "block";
+	  x.parentElement.classList.remove('no-print')
 	} else {
 	  x.style.display = "none";
+	  x.parentElement.classList.add('no-print')
 	}
   }
 
